5e14073d0f3d75c59fd73dec54b48a8e61415eb4
Adron Hall <adronhall@gmail.com>
2020-11-09 19:26:14 -0800

    Some things works. Ugh, fucking paradigm change from one to another on the fundamental notion of referencing a CONTROL!

--- x-plat-ui/EmpireBuilder/EmpireBuilder/MainWindow.axaml.cs
+++ x-plat-ui/EmpireBuilder/EmpireBuilder/MainWindow.axaml.cs
@@ -1,11 +1,29 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using GraphQL;
+using GraphQL.Types;
 using SharpDX.Direct3D11;
+using GraphQL.Client;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
 
 namespace EmpireBuilder
 {
+    public class Railroad
+    {
+        public string Name { get; set; }
+        public string ServiceArea { get; set; }
+        public string Uri { get; set; }
+        public string WikipediaUri { get; set; }
+        public string MapLink { get; set; }
+        public string HqCity { get; set; }
+        public string History { get; set; }
+        public Guid Id { get; set; }
+    }
+    
     public class MainWindow : Window
     {
         public MainWindow()
@@ -23,7 +41,21 @@ namespace EmpireBuilder
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            var graphQLClient = new GraphQLHttpClient("http://localhost:8080/v1/graphql", new NewtonsoftJsonSerializer());
+            var railraodsRequest = new GraphQLRequest {
+                Query = @"
+                {
+                  logistics_Railraod {
+                    name
+                    hqCity
+                    mapLink
+                    serviceArea
+                  }
+                }"
+            };
+
+            var responseText = this.FindControl<TextBox>("RailroadResponse");
+            responseText.Text = "test";
         }
     }
 }
